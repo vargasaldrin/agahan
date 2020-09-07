51a810440340f81a191324cd5a567c0cf9417aa1
vargasaldrin <vargasero@gmail>
2020-09-07 16:38:36 +0800

    first commit

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/header.css'
+
+export default function Header(props) {
+
+    return (
+        <>
+            <header>Yow</header>
+            <h1>asdasd</h1>
+        </>
+    )
+}
